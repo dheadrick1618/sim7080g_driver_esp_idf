b88d90cf66c55390c04e2be558f0b0fa3d5c4b21
Devin <headrick@ualberta.ca>
2024-10-22 16:25:50 -0600

    Setup UART driver init

--- include/sim7080g_driver_esp_idf.h
+++ include/sim7080g_driver_esp_idf.h
@@ -1,3 +1,53 @@
 #pragma once
 
-void func(void);
+#include <esp_err.h>
+#include <stdbool.h>
+
+#define SIM7080G_UART_BAUD_RATE 115200
+#define SIM87080G_UART_BUFF_SIZE 1024
+
+// TODO - Check these values against  MQTT v3 protocol specs
+#define MQTT_BROKER_URL_MAX_CHARS 128
+#define MQTT_BROKER_USERNAME_MAX_CHARS 32
+#define MQTT_BROKER_CLIENT_ID_MAX_CHARS 32
+#define MQTT_BROKER_PASSWORD_MAX_CHARS 32
+
+/// @brief UART config struct defined by user of driver and passed to driver init
+/// @note TX and RX here are in the perspective of the SIM7080G, and thus they are swapped in the perspecive of the ESP32
+typedef struct
+{
+    int gpio_num_tx;
+    int gpio_num_rx;
+    int port_num; // This was chosen to be an INT because the esp-idf uart driver takes an int type
+} sim7080g_uart_config_t;
+
+/// @brief For config device with MQTT broker.
+/// @note Thinger refers to the client password as the 'device credentials'
+typedef struct
+{
+    char broker_url[MQTT_BROKER_URL_MAX_CHARS];
+    char username[MQTT_BROKER_USERNAME_MAX_CHARS];
+    char client_id[MQTT_BROKER_CLIENT_ID_MAX_CHARS];
+    char client_password[MQTT_BROKER_PASSWORD_MAX_CHARS];
+    uint16_t port;
+} sim7080g_mqtt_config_t;
+
+typedef struct
+{
+    sim7080g_uart_config_t uart_config;
+    sim7080g_mqtt_config_t mqtt_config;
+    bool initialized;
+} sim7080g_handle_t;
+
+/// @brief Creates a device handle with the provided configuration
+/// @note This must be called before a device handle can be used
+/// @param sim7080g_handle
+/// @param sim7080g_uart_config
+/// @param sim7080g_mqtt_config
+/// @return
+esp_err_t sim7080g_init(sim7080g_handle_t *sim7080g_handle,
+                        const sim7080g_uart_config_t sim7080g_uart_config,
+                        const sim7080g_mqtt_config_t sim7080g_mqtt_config);
+
+esp_err_t sim7080g_deinit(sim7080g_handle_t *sim7080g_handle);
+
--- sim7080g_driver_esp_idf.c
+++ sim7080g_driver_esp_idf.c
@@ -1,7 +1,65 @@
 #include <stdio.h>
+#include <esp_err.h>
+#include <esp_log.h>
+#include <driver/uart.h>
+
 #include "sim7080g_driver_esp_idf.h"
 
-void func(void)
-{
+static const char *TAG = "SIM7080G Driver";
 
+// Static Fxn Declarations:
+static esp_err_t sim7080g_config_uart(const sim7080g_uart_config_t sim7080g_uart_config);
+
+// ---------------------  EXTERNAL EXPOSED API FXNs  -------------------------//
+esp_err_t sim7080g_init(sim7080g_handle_t *sim7080g_handle,
+                        const sim7080g_uart_config_t sim7080g_uart_config,
+                        const sim7080g_mqtt_config_t sim7080g_mqtt_config)
+{
+    esp_err_t err = sim7080g_config_uart(sim7080g_uart_config);
+    if(err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Error configuring UART: %s", esp_err_to_name(err));
+        return err;
+    }
+
+    // config mqtt placeholder
+
+    return ESP_OK;
 }
+
+static esp_err_t sim7080g_config_uart(const sim7080g_uart_config_t sim7080g_uart_config)
+{
+    uart_config_t uart_config = {
+        .baud_rate = SIM7080G_UART_BAUD_RATE,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_DEFAULT,
+    };
+
+    esp_err_t err = uart_driver_install((uart_port_t)sim7080g_uart_config.port_num, SIM87080G_UART_BUFF_SIZE * 2, 0, 0, NULL, 0);
+    if(err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Error installing UART driver: %s", esp_err_to_name(err));
+        return err;
+    }
+
+    err = uart_param_config((uart_port_t)sim7080g_uart_config.port_num, &uart_config);
+    if(err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Error configuring UART parameters: %s", esp_err_to_name(err));
+        return err;
+    }
+
+    err = uart_set_pin((uart_port_t)sim7080g_uart_config.port_num, sim7080g_uart_config.gpio_num_rx, sim7080g_uart_config.gpio_num_tx, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+    if(err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Error setting UART pins: %s", esp_err_to_name(err));
+        return err;
+    }
+
+    return ESP_OK;
+}
+
+// config mqtt placeholder
